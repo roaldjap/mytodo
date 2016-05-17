e09fb51780da57cc6f343140bea1c0a1ef1f4601
Roald Ignacio <roaldjap@gmail.com>
2016-05-17 18:02:42 +0800

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mytodoApp
+ * @description
+ * # mytodoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mytodoApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+
+    // Add Todo List
+    $scope.addTodo = function(){
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+
+    //Remove Todo List
+    $scope.removeTodo = function(index){
+      $scope.todos.splice(index, 1);
+    }
+  });
